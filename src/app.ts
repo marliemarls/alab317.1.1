0049351a350f063fec20e13bd115bf123ed4c505
Marlie Martinez <marlieahue@gmail.com>
2024-06-25 10:36:40 -0400

    ughhhhhhh

--- src/app.ts
+++ src/app.ts
@@ -48,21 +48,20 @@ myBuick.wheels = myBuick.wheels - 1;
 console.log(myBuick.wheels);
 console.log(myBuick.model);
 
-class NCycle {
-  make: string | unknown[];
-  model: string | unknown[];
+class NCycle <Type> {
+  make: Type | Type[];
+  model: Type | Type[];
   wheels: number;
-  status: string;
+  status: status = "started";
   constructor(
-    make: string | unknown[],
-    model: string | unknown[],
+    make: Type | Type[],
+    model: Type | Type[],
     wheels: number,
-    status: string
+    status: status = "started"
   ) {
     this.make = make;
     this.model = model;
     this.wheels = wheels;
-    this.status = status;
   }
   start() {
     this.status = "started";
@@ -70,8 +69,7 @@ class NCycle {
   stop() {
     this.status = "stopped";
   }
-  printMethod <Type>(
-    wheels:number,
+  print <Type>(
     number: number = 0,
     make: Type | Type[],
     model: Type | Type[]
@@ -89,3 +87,34 @@ class NCycle {
 }
 
 
+// // Rudimentary testing Code, not part of the assignment
+// const testCycle1 = new NCycle<number>(1, 2, 3);
+// testCycle1.print();
+// testCycle1.printAll();
+
+// const testCycle2 = new NCycle<string>("This", "That", 4);
+// testCycle2.print();
+// testCycle2.printAll();
+
+// const testCycle3 = new NCycle<string>("Make", 10, 4);
+// testCycle3.print(4);
+// testCycle3.printAll();
+
+// const makes4 = ["Volkswagon", "Tesla", "Audi"];
+// const models4 = ["Passat", "Model X", "A4"];
+// const testCycle4 = new NCycle<string[]>(makes4, models4, 4);
+// testCycle4.print(2);
+// testCycle4.printAll();
+
+// const makes5 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const models5 = [1, 1, 2, 3, 5];
+// const testCycle5 = new NCycle<number[]>(makes5, models5, 0);
+// testCycle5.print(7);
+// testCycle5.printAll();
+
+// function add(x: number, y: number): number {
+//   return x + y;
+// }
+// add(testCycle1.make, testCycle5.model[1]);
+// // Error expected here
+// add(testCycle2.make, testCycle4.model[1]);

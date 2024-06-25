61802ce9c397de2b7a7d7dbdc3f9a0f71067aac1
Marlie Martinez <marlieahue@gmail.com>
2024-06-25 09:58:27 -0400

    ugh

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
+type status = "started" | "stopped";
 class Vehicle {
+  status: status = "stopped";    
   make: string;
   model: string;
   wheels: number;
-  status: string;
 
-  constructor(make: string, model: string, wheels: number, status: string) {
-    this.status = status;
+
+  constructor(make: string, model: string, wheels: number) {
     this.make = make;
     this.model = model;
     this.wheels = wheels;
@@ -19,31 +20,51 @@ class Vehicle {
 }
 
 class Car extends Vehicle {
-  constructor(make: string, model: string,wheels:number, status:string) {
-    super(make, model, 4, "running");
+  constructor(make: string, model: string, wheels: number) {
+    super(make, model, 4);
   }
 }
 
 class MotorCycle extends Vehicle {
-  constructor(make: string, model: string,wheels:number, status:string) {
-    super(make, model, 2, "running");
+  constructor(make: string, model: string, wheels: number) {
+    super(make, model, 2);
   }
 }
 
-function printStatus(vehicle: Vehicle) {
-  if (vehicle.status === "running") {
+function printStatus(vehicle: Vehicle):void {
+  if (vehicle.status === "started") {
     console.log("The vehicle is running.");
   } else {
     console.log("The vehicle is stopped.");
   }
 }
 
-const myHarley = new MotorCycle("Harley-Davidson", "Low Rider S", 2, "stopped");
+const myHarley = new MotorCycle("Harley-Davidson", "Low Rider S", 2);
 myHarley.start();
 printStatus(myHarley);
 console.log(myHarley.make.toUpperCase());
 
-const myBuick = new Car("Buick", "Regal", 4,"running");
+const myBuick = new Car("Buick", "Regal", 4);
 myBuick.wheels = myBuick.wheels - 1;
 console.log(myBuick.wheels);
 console.log(myBuick.model);
+
+class NCycle {
+  make: string | string[];
+  model: string | string[];
+  wheels: number;
+  status: string;
+  constructor(make: string | string [], model: string | string [], wheels: number, status:string) {
+    this.make = make;
+    this.model = model;
+    this.wheels = wheels;
+    this.status = status;
+}
+printMethod(number:number = 0, make: string | unknown [], model: string | unknown []) : void{
+   if(typeof make !== String)
+}
+}
+
+printAll() : void{
+    
+}
